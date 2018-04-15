1c1b6ddd8386ad3a53e8cd8c4cfb394a572fbbfa
craig <cisenor1@gmail.com>
2018-04-15 15:10:06 -0700

    Look! Tests!

---
+++ tests/entities/components/hunger_spec.lua
@@ -0,0 +1,20 @@
+describe("an entity with hunger", function()
+  local goblin = require('game.entities.goblin'):new()
+  it('should expose a public hunger value', function()
+    pending('TODO')
+  end)
+
+  it('should lose hunger over time', function()
+    pending('TODO')
+  
+  end)
+
+  it('should lose more hunger while doing strenuous activities', function()
+    pending('TODO')
+  
+  end)
+
+  it('should be able to replenish their hunger by eating', function()
+    pending('TODO')
+  end)
+end)
