5abec8c92957553be04e23dbfdde2289e10091b7
craig <cisenor1@gmail.com>
2018-04-15 15:32:15 -0700

    More spec stuff

--- tests/entities/components/hunger_spec.lua
+++ tests/entities/components/hunger_spec.lua
@@ -1,7 +1,16 @@
 describe("an entity with hunger", function()
-  local goblin = require('game.entities.goblin'):new()
-  it('should expose a public hunger value', function()
-    pending('TODO')
+  local config = require('config')()
+  local Vector2 = require('game.point')
+  local Goblin = require('game.entities.goblin')
+  local goblin = Goblin:new(config.goblins, Vector2:new(0, 0), {})
+
+  it('should expose a total hunger value', function()
+    assert.is_not_nil(goblin.totalHunger)
+  end)
+
+  it('should expose a current hunger value', function()
+    assert.is_not_nil(goblin.currentHunger)
+    assert.is_true(type(goblin.currentHunger) == 'number')
   end)
 
   it('should lose hunger over time', function()
